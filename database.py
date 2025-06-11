e1519240729e679430920ec443876fe4daca5d54
13byte <13bitadc@naver.com>
2025-06-11 20:20:55 +0900

    feat: DB 연결정보 환경변수 설정 적용

--- database.py
+++ database.py
@@ -1,8 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:@/fastapi_ca"
+# .env 파일 로드
+load_dotenv()
+
+# 환경변수에서 DB 설정 가져오기
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "fastapi_ca")
+
+# 데이터베이스 URL 구성
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- migrations/env.py
+++ migrations/env.py
@@ -11,6 +11,8 @@ import database_models
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option("sqlalchemy.url", database.SQLALCHEMY_DATABASE_URL)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
